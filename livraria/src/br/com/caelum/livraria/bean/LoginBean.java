a72db3db0e37c5c58af59efa89d4b0bd497c4aa7
fabio <fabiormiranda@hotmail.com>
2017-11-06 22:05:52 -0200

    Projeto Alura JSF1

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,8 +2,10 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.dao.UsuarioDao;
@@ -23,15 +25,26 @@ public class LoginBean {
 	public String efetuaLogin(){
 		System.out.println("Fazendo login do usuário" + this.usuario.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe){
+			
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		
-		return null;
+			context.getExternalContext().getFlash().setKeepMessages(true);
+			context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		
+			return "login?faces-redirect=true";
 		
 	}
 
-	
+	public String deslogar(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
+		
+	}
 	
 }
